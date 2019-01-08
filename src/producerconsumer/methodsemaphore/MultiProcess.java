4a5cca696e96006705807b152b8ae3289afe1dcf
zhangjp6 <zhangjp6@yonyou.com>
2019-01-08 10:27:01 +0800

    生产者增加定量生产控制

--- src/producerconsumer/methodsemaphore/MultiProcess.java
+++ src/producerconsumer/methodsemaphore/MultiProcess.java
@@ -4,8 +4,8 @@ public class MultiProcess {
     public static void main(String[] args) {
         MyRepository repository = new MyRepository(10);
         //生产者
-        new Thread(new MyProducer("woker1",repository)).start();
-        new Thread(new MyProducer("woker2",repository)).start();
+        new Thread(new MyProducer("woker1",repository,true)).start();
+//        new Thread(new MyProducer("woker2",repository,true)).start();
 
         //消费者
         new Thread(new MyConsumer("consumer1",repository)).start();
--- src/producerconsumer/methodsemaphore/MyProducer.java
+++ src/producerconsumer/methodsemaphore/MyProducer.java
@@ -4,12 +4,20 @@ public class MyProducer implements Runnable {
 
     private String name;
     private MyRepository repository;
-    private Boolean isStop;
+    private Boolean isStop = false;
+    private int capability;
 
+    /**
+     * 生产者是否定量生产
+     * @param name
+     * @param repository
+     * @param isStop
+     */
     public MyProducer(String name,MyRepository repository,Boolean isStop){
         this.name = name;
         this.repository = repository;
         this.isStop = isStop;
+        this.capability = repository.getCapability();
     }
 
     public MyProducer(String name,MyRepository repository){
@@ -19,12 +27,24 @@ public class MyProducer implements Runnable {
 
     @Override
     public void run() {
-        while (true){
-            try {
-                repository.add(1,this.name);
-                Thread.sleep(100);
-            }catch (InterruptedException e){
-                e.printStackTrace();
+        if(isStop){
+            while (capability > 0){
+                try {
+                    repository.add(1,this.name);
+                    capability--;
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }else {
+            while (true){
+                try {
+                    repository.add(1,this.name);
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
             }
         }
 
